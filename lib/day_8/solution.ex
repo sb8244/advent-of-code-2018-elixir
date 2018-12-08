b4f0ddf8cba544a5667cad8c4909bca6593949fb
Stephen Bussey <steve.bussey@salesloft.com>
2018-12-08 15:36:48 -0500

    Day 8 solutions

--- lib/day_8/solution.ex
+++ lib/day_8/solution.ex
@@ -1,35 +1,87 @@
 defmodule Advent.Day8.Solution do
   defmodule Node do
-    @enforce_keys [:num_children, :num_metadata, :metadata, :children]
-    defstruct @enforce_keys
+    @enforce_keys [:num_children, :num_metadata]
+    defstruct @enforce_keys ++ [metadata: [], children: []]
   end
-
-  # 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
-  # A----------------------------------
-  #     B----------- C-----------
-  #                     D-----
-
-  2 3 *0 3 10 11 12* *1 1 0 1 99 2* 1 1 2
-  0 3 10 11 12
-  1 1 *0 1 99* 2
-  0 1 99
 
   def tree(input) do
-    tree(String.split(input, " "), parent: nil)
+    {[], tree} = tree(String.split(input, " "), parent: nil)
+    tree
   end
 
-  def tree(list, parent: parent_node) when list == [], do: parent_node
+  def reduce(tree = %Node{children: children}, acc, func) do
+    acc = func.(tree, acc)
+    Enum.reduce(children, acc, fn child, acc ->
+      reduce(child, acc, func)
+    end)
+  end
 
-  def tree([num_children | [num_metadata | rest]], parent: parent_node) do
+  def sum_by_metadata_entries(%Node{children: children, metadata: metadata}, acc) when children == [] do
+    sum =
+      metadata
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+
+    acc + sum
+  end
+
+  def sum_by_metadata_entries(%Node{children: children, metadata: metadata}, acc) do
+    child_sums =
+      children
+      |> Enum.map(fn child ->
+        sum_by_metadata_entries(child, 0)
+      end)
+      |> Enum.reverse()
+
+    sum =
+      metadata
+      |> Enum.map(& String.to_integer(&1) - 1)
+      |> Enum.reduce(0, fn index, acc ->
+        Enum.at(child_sums, index, 0) + acc
+      end)
+
+    acc + sum
+  end
+
+  # private
+
+  defp tree(["0" | [num_metadata | rest]], parent: parent) do
+    num_metadata = String.to_integer(num_metadata)
+    metadata = Enum.take(rest, num_metadata)
+    without_metadata = Enum.drop(rest, num_metadata)
+
+    node = %Node{num_children: 0, num_metadata: num_metadata, metadata: metadata}
+    new_parent_children = [node | parent.children]
+    new_parent = Map.put(parent, :children, new_parent_children)
+
+    {without_metadata, new_parent}
+  end
+
+  defp tree([num_children | [num_metadata | rest]], parent: parent_node) do
     num_children = String.to_integer(num_children)
     num_metadata = String.to_integer(num_metadata)
+    node = %Node{num_children: num_children, num_metadata: num_metadata}
 
-    metadata = Enum.take(rest, -num_metadata)
-    trimmed_rest = Enum.drop(rest, -num_metadata)
+    {rest, node} =
+      Enum.reduce(times(num_children), {rest, node}, fn _, {rest, parent} ->
+        tree(rest, parent: parent)
+      end)
 
-    IO.inspect {num_children, num_metadata, rest}
+    this_metadata = Enum.take(rest, num_metadata)
+    without_metadata = Enum.drop(rest, num_metadata)
+    new_node = Map.put(node, :metadata, this_metadata)
 
-    my_node = %Node{num_children: num_children, num_metadata: num_metadata, metadata: metadata, children: []}
-    tree(trimmed_rest, parent: my_node)
+    new_parent = if parent_node do
+      new_parent_children = [new_node | parent_node.children]
+      Map.put(parent_node, :children, new_parent_children)
+    else
+      new_node
+    end
+
+    {without_metadata, new_parent}
+  end
+
+  defp times(n) do
+    for i <- 0..n, i > 0, do: i
   end
 end
